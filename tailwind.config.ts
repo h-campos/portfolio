865a1a520b7ff793a6062011efb7f9f3ddb0de3b
hcampos <hcampospro@gmail.com>
2024-02-20 23:50:52 +0100

    chore(app): remove favicon.ico file and update global CSS variables for color scheme feat(app): update layout to use French language and adjust metadata for Hugo CAMPOS feat(tailwind.config): update content paths and extend theme with new colors and border radius

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,31 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}"
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        background: "hsl(var(--background))",
+        primary: "hsl(var(--primary))",
+        secondary: "hsl(var(--secondary))",
+        muted: "hsl(var(--muted))",
+        accent: "hsl(var(--accent))",
+        popover: "hsl(var(--popover))",
+        card: "hsl(var(--card))"
       },
-    },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
