b433ed639c88a92545d7dcf8b397be52e46057bd
hcampos <hcampospro@gmail.com>
2024-02-21 00:14:18 +0100

    style(page.tsx): add a button element with a test class for styling purposes to the Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import type { ReactElement } from "react";
 
 export default function Home(): ReactElement {
   return (
-    <div>Hello world</div>
+    <div>Hello world
+      <button className="bg-secondary px-2 py-3 rounded-md">TEST</button>
+    </div>
   );
 }
