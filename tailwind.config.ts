d01d727bcb2f93b17acb8d4c2a764f9c3ec7acdc
hcampos <hcampospro@gmail.com>
2024-02-21 00:05:07 +0100

    chore: add Space Grotesk font to project for better typography and design consistency chore: remove unused Next.js and Vercel SVG files from the project feat: update page component to use Space Grotesk font and improve layout styling feat: update Tailwind CSS config to include Space Grotesk font family and extend theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        spaceGrotesk: "Space Grotesk, Roboto, sans-serif"
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
