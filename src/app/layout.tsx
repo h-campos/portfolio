767e4d7ba9a35fe3763030f1196dfeeabd050313
hcampos <hcampospro@gmail.com>
2024-02-21 16:26:59 +0100

    chore(layout.tsx): add support for viewport metadata to define theme color feat(page.tsx): refactor Home component to return a simple JSX fragment feat(about.section.tsx): create AboutSection component feat(component.ts): add types for Component, AsyncComponent, and SetState feat(index.ts): export Component types from component.ts file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import type { ReactElement } from "react";
+import { cn } from "@/lib/utils/clsx";
 
 export const metadata: Metadata = {
   title: "Hugo CAMPOS | Développeur FrontEnd",
   description: "Développeur FrontEnd depuis 4 ans, avec une aisance dans l'écosystème Javascript/Typescript"
+};
+
+export const viewport: Viewport = {
+  themeColor: "#1c1917"
 };
 
 export default function RootLayout({
@@ -14,8 +19,13 @@ export default function RootLayout({
 }>): ReactElement {
   return (
     <html lang="fr">
-      <body className="min-h-screen bg-background scroll-smooth py-16 px-16">
-        <main>
+      <body
+        className={cn(
+          "min-h-screen bg-background scroll-smooth",
+          "py-12 sm:py-16 px-8 sm:px-20 md:px-32 lg:px-60 xl:px-80",
+          "text-primary font-spaceGrotesk"
+        )}>
+        <main className="">
           {children}
         </main>
       </body>
---
+++ src/lib/sections/about.section.tsx
@@ -0,0 +1,7 @@
+import type { ReactElement } from "react";
+
+export default function AboutSection(): ReactElement {
+  return (
+    <div></div>
+  );
+}
