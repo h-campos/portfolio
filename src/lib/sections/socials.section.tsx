683c4574fe3d16462e0b7a233db8e530e6e6f89e
hcampos <hcampospro@gmail.com>
2024-02-23 15:50:09 +0100

    fix(socials.section.tsx): update Malt profile link to point to correct profile feat(socials.section.tsx): add Bento profile link to the socials section

--- src/lib/sections/socials.section.tsx
+++ src/lib/sections/socials.section.tsx
@@ -6,7 +6,8 @@ export default function SocialsSection(): ReactElement {
     <div className="flex flex-wrap gap-2">
       <LinkButton href="https://github.com/Sakoutecher" target="_blank">Github</LinkButton>
       <LinkButton href="https://www.linkedin.com/in/hcamposdev" target="_blank">Linkedin</LinkButton>
-      <LinkButton href="https://www.malt.fr/profile/hcampos" target="_blank">Malt</LinkButton>
+      <LinkButton href="https://www.malt.fr/profile/hugocampos" target="_blank">Malt</LinkButton>
+      <LinkButton href="https://bento.me/hcampos" target="_blank">Bento</LinkButton>
       <LinkButton href="mailto:hcampospro@gmail.com" target="_blank">Email</LinkButton>
     </div>
   );
