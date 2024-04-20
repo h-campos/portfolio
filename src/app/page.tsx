e7a5f0ad6bb66b4352f6af6bb9b4de3ef2015547
hcampos <hcampospro@gmail.com>
2024-04-20 14:19:11 +0200

    chore(page.tsx): remove unnecessary TODO comment before exporting Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import SkillsSection from "@/lib/sections/skills.section";
 import SocialsSection from "@/lib/sections/socials.section";
 import ExperiencesSection from "@/lib/sections/experiences.section";
 
+//TODO: Do some test
+
 export default function Home(): ReactElement {
   return (
     <>
