2f12ece6e1129d36512f12e88dca1bea97307500
hcampos <hcampospro@gmail.com>
2025-03-29 01:08:46 +0100

    feat(next.config.mjs): add support for unoptimized images in Next.js configuration to improve performance

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true
+  },
+
   ...(process.env.NEXTJS_STANDALONE_OUTPUT !== undefined ? { output: "standalone" } : {}),
 };
 
